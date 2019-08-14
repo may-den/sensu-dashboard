d3308d2d300ed416ae6eedc8be17b4a6fa171b09
Jake Shepherd <jacob.shepherd@mayden.co.uk>
2019-08-14 16:56:46 +0100

    UI Improvements
    Shortened names, removed 'am'/'pm', added date

--- js/components/display/DisplaySensorStatus.js
+++ js/components/display/DisplaySensorStatus.js
@@ -10,20 +10,28 @@ const DisplaySensorStatus = ({data}) => {
             <div className="grid-container">
                 <div className="nameHeading">Name</div>
                 <div className="statusHeading">Status</div>
+                <div className="statusCodeHeading">(Code)</div>
                 <div className="timeHeading">Time</div>
+                <div className="messageHeading">Message</div>
             </div>
             {
                 data.map(datum => {
+                    var dateString = new Date(datum.check.executed*1000).toLocaleDateString();
                     var date = new Date(datum.check.executed * 1000);
                     let time = date.getHours() + ':' + ("0" + date.getMinutes()).substr(-2) + ':' + ("0" + date.getSeconds()).substr(-2);
 
                     let statusColour = datum.check.status === 0 ? "statusItemGreen" : "statusItemRed";
+                    let printMessage = statusColour === "statusItemGreen" ?  "" : datum.check.output;
+
+                    let name = datum.check.name.replace("Mayden.Sensors.", "");
 
                     return (
                         <div className="grid-container">
-                            <div className={statusColour + "NameItem"}>{datum.check.name}</div>
-                            <div className={statusColour}>{datum.check.status}</div>
-                            <div className="timeItem">{time}</div>
+                            <div className="nameItem">{name}</div>
+                            <div className ={statusColour + "Dot"}/>
+                            <div className="statusCodeItem">({datum.check.status})</div>
+                            <div className="timeItem">{time + " " + dateString}</div>
+                            <div className="messageItem">{printMessage}</div>
                         </div>
                     )
                 })
