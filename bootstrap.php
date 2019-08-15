79bd66c45ffad656c330bb7f0cbe797894d8c49a
dbould <dave@bould.net>
2019-08-15 13:57:51 +0100

    Move config container assignment out of bootstrap So it works

--- bootstrap.php
+++ bootstrap.php
@@ -18,13 +18,3 @@ $middleware($app);
 // Register routes
 $routes = require __DIR__ . '/src/routes.php';
 $routes($app);
-
-// Register Config
-if (file_exists(__DIR__ . '/config.json')) {
-    $configJson = file_get_contents(__DIR__ . '/config.json');
-} else {
-    die("Couldn't find config.json\n");
-}
-
-$config = json_decode($configJson, 1);
-$container['config'] = $config;
