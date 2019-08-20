5a89dad1ac2b20181d83635b8b97140425560780
Jake Shepherd <jacob.shepherd@mayden.co.uk>
2019-08-20 16:27:35 +0100

    Added more snapshot tests

--- js/__tests__/DisplaySensorStatus.test.js
+++ js/__tests__/DisplaySensorStatus.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import renderer from "react-test-renderer";
 import DisplaySensorStatus from "../components/display/DisplaySensorStatus";
 
+const noData = []
+
 const data =
     [{
         "client": "sensu-client-ivnode14",
@@ -22,29 +24,73 @@ const data =
             "output": "sensu.iaptus.bracknell.iaptusvnode14.availability_count.12_gloucester 1 1563781830\n...",
             "status": 0
         }
-    },
-        {
-            "client": "sensu-client-cron01.iaptus.ukc",
-            "check": {
-                "command": "php /var/www/html/sensor-manager/SensorManager.php run Iaptus Sms\\\\Checks\\\\SendingOnTime",
-                "subscribers": [
-                    "iaptus"
-                ],
-                "interval": 1800,
-                "handlers": [
-                    "mailer",
-                    "slack"
-                ],
-                "name": "Mayden.Sensors.Iaptus.Sms.Checks.SendingOnTime",
-                "issued": 1562772496,
-                "executed": 1562772513,
-                "duration": 13.33,
-                "output": "{\"errors\":[{\"value\":\"1\"},{\"value\":\"16\"}]}\n",
-                "status": 2,
-                "type": "standard"
-            }
-        },
-    ]
+    }]
+
+const noNameData = [
+    {
+        "client": "sensu-client-ivnode14",
+        "check": {
+            "type": "metric",
+            "command": "php /var/www/html/sensor-manager/SensorManager.php run Iaptus Availabilities\\\\Metrics\\\\AvailabilityCreatedCount",
+            "subscribers": [
+                "iaptus"
+            ],
+            "interval": 450,
+            "handlers": [
+                "relay"
+            ],
+            "issued": 1563781786,
+            "executed": 1563781813,
+            "duration": 17.94,
+            "output": "sensu.iaptus.bracknell.iaptusvnode14.availability_count.12_gloucester 1 1563781830\n...",
+            "status": 0
+        }
+    }
+]
+
+const noStatusData = [
+    {
+        "client": "sensu-client-ivnode14",
+        "check": {
+            "type": "metric",
+            "command": "php /var/www/html/sensor-manager/SensorManager.php run Iaptus Availabilities\\\\Metrics\\\\AvailabilityCreatedCount",
+            "subscribers": [
+                "iaptus"
+            ],
+            "interval": 450,
+            "handlers": [
+                "relay"
+            ],
+            "name": "Mayden.Sensors.Iaptus.Availabilities.Metrics.AvailabilityCreatedCount",
+            "issued": 1563781786,
+            "executed": 1563781813,
+            "duration": 17.94,
+            "output": "sensu.iaptus.bracknell.iaptusvnode14.availability_count.12_gloucester 1 1563781830\n...",
+        }
+    }
+]
+
+const noExecutedData = [
+    {
+        "client": "sensu-client-ivnode14",
+        "check": {
+            "type": "metric",
+            "command": "php /var/www/html/sensor-manager/SensorManager.php run Iaptus Availabilities\\\\Metrics\\\\AvailabilityCreatedCount",
+            "subscribers": [
+                "iaptus"
+            ],
+            "interval": 450,
+            "handlers": [
+                "relay"
+            ],
+            "name": "Mayden.Sensors.Iaptus.Availabilities.Metrics.AvailabilityCreatedCount",
+            "issued": 1563781786,
+            "duration": 17.94,
+            "output": "sensu.iaptus.bracknell.iaptusvnode14.availability_count.12_gloucester 1 1563781830\n...",
+            "status": 0
+        }
+    }
+]
 
 describe('Display Component', () => {
     it('matches the snapshot', () => {
@@ -53,4 +99,33 @@ describe('Display Component', () => {
             .toJSON()
         expect(tree).toMatchSnapshot()
     })
+
+    it('does not have data', () => {
+        const tree = renderer
+            .create(<DisplaySensorStatus data = {noData}/>)
+            .toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+
+    it('does not have name data', () => {
+        const tree = renderer
+            .create(<DisplaySensorStatus data ={noNameData}/>)
+            .toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+
+    it('does not have status data', () => {
+        const tree = renderer
+            .create(<DisplaySensorStatus data = {noStatusData}/>)
+            .toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+
+    it('does not have executed data', () => {
+        const tree = renderer
+            .create(<DisplaySensorStatus data = {noExecutedData}/>)
+            .toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+
 })
--- js/components/display/DisplaySensorStatus.js
+++ js/components/display/DisplaySensorStatus.js
@@ -7,33 +7,49 @@ const DisplaySensorStatus = ({data}) => {
         return <div/>
     }
 
-    function getService(name)
-    {
+    function getService(name) {
         return (name.split(".", 2)).join(".");
     }
 
-    function getName(datum)
-    {
-        return datum.check.name.replace("Mayden.Sensors.", "");
+    function getName(datum) {
+        if (!datum.check.hasOwnProperty('name')) {
+            return "NO_NAME"
+        } else {
+            return datum.check.name.replace("Mayden.Sensors.", "");
+        }
     }
 
-    function getStatusColour(datum)
-    {
-        return datum.check.status === 0 ? "statusItemGreen" : "statusItemRed";
+    function getStatusColour(datum) {
+        if(!datum.check.hasOwnProperty('status')){
+            return "statusItemRed"
+        }
+        else{
+            return datum.check.status === 0 ? "statusItemGreen" : "statusItemRed";
+        }
     }
 
-    function getDateTime(datum)
-    {
-        let date = new Date(datum.check.executed * 1000);
-        let dateString = date.toLocaleDateString();
-        let time = date.getHours() + ':' + ("0" + date.getMinutes()).substr(-2) + ':' + ("0" + date.getSeconds()).substr(-2);
+    function getDateTime(datum) {
+        if(!datum.check.hasOwnProperty('executed')){
+            return "NO_TIME"
+        }else{
+            let date = new Date(datum.check.executed * 1000);
+            let dateString = date.toLocaleDateString();
+            let time = date.getHours() + ':' + ("0" + date.getMinutes()).substr(-2) + ':' + ("0" + date.getSeconds()).substr(-2);
 
-        return time + " " + dateString;
+            return time + " " + dateString;
+        }
     }
 
-    function getMessage(datum)
-    {
-        return getStatusColour(datum) === "statusItemRed" ?  datum.check.output : "";
+    function getMessage(datum) {
+        return getStatusColour(datum) === "statusItemRed" ? datum.check.output : "";
+    }
+
+    function processName(datum) {
+        if (!datum.check.hasOwnProperty('name')) {
+            return "NO_NAME"
+        } else {
+            return getName(datum).replace(getService(getName(datum)) + ".", "")
+        }
     }
 
     return (
@@ -53,8 +69,8 @@ const DisplaySensorStatus = ({data}) => {
                     return (
                         <div className="grid-container" key={index}>
                             <div className="serviceItem">{getService(getName(datum))}</div>
-                            <div className="nameItem">{getName(datum).replace(getService(getName(datum)) + ".", "")}</div>
-                            <div className ={getStatusColour(datum) + "Dot"}/>
+                            <div className="nameItem">{processName(datum)}</div>
+                            <div className={getStatusColour(datum) + "Dot"}/>
                             <div className="statusCodeItem">({datum.check.status})</div>
                             <div className="timeItem">{getDateTime(datum)}</div>
                             <div className="messageItem">{getMessage(datum)}</div>
